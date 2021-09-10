0d133f36eca7e81a293491fd0f48c046d158f5e8
hongbozhong <hzhong1@ualberta.ca>
2021-09-10 23:39:17 +0800

    download file itself

--- curl_script.py
+++ curl_script.py
@@ -1,3 +1,4 @@
 import requests
-res = requests.get("http://google.com/", allow_redirects=True)
+res = requests.get("https://raw.githubusercontent.com/hongbozhong/404-Lab1/master/curl_script.py", allow_redirects=True)
+open("curl_script_copy.py", "wb").write(res.content)
 print(res.text)
